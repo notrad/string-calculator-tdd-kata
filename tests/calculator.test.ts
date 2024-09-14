dc8a7cdb2b52e40705a3eb398fa47bf9ae5dfe55
notrad <anoopbevendcunha@gmail.com>
2024-09-14 21:23:02 +0530

    added test case: input 0 should return 0

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -6,8 +6,12 @@ describe('String calculator TDD kata', () => {
     expect(calculate("")).toBe(0);
   });
 
+  test('input "0" should return 0' , () => {
+    expect(calculate("0")).toBe(0);
+  });
+
   test('input "1" should return 1' , () => {
     expect(calculate("1")).toBe(1);
-  })
+  });
 });
 
