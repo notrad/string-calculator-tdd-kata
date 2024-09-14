1b2a937e1554e059f3fb8ef000f24619738673e1
notrad <anoopbevendcunha@gmail.com>
2024-09-15 00:07:12 +0530

    passed test case: numbers above 1000 must be ignored

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,22 +1,18 @@
 export function calculate(numbers: string): number {
-    let sum: number = 0;
     let delimiter: string = ",";
     let negativeNumbers: number[] = [];
+    let parsedNumbers: number[] = [];
 
-    // Setting dynamic delimiter
     if (numbers.startsWith("//")) {
         delimiter = numbers.charAt(2);
         numbers = numbers.slice(2)
     }
 
-    numbers.replace(/["\n"]/g, delimiter).split(delimiter).forEach((character: string) => {
-        if (Number(character) < 0) {
-            negativeNumbers.push(Number(character));
-        }
-        sum += Number(character);
-    });
+    parsedNumbers = numbers.replace(/["\n"]/g, delimiter).split(delimiter).map(Number).filter((number:number) => number < 1000 || Number.isNaN(number));
+
+    negativeNumbers = parsedNumbers.filter((number:number) => number < 0);
 
     if (negativeNumbers.length > 0) throw new Error(`negative numbers not allowed ${negativeNumbers.toString()}`);
 
-    return sum;
+    return parsedNumbers.reduce((acc, num) => acc + num, 0);;
 }
