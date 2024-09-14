2f003caeae955d0201c2f349c5200d3bc57766f9
notrad <anoopbevendcunha@gmail.com>
2024-09-14 23:20:30 +0530

    passing test: variable length negative numbers not allowed

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,6 +1,7 @@
 export function calculate(numbers: string): number {
     let sum: number = 0;
     let delimiter: string = ",";
+    let negativeNumbers: number[] = [];
 
     // Setting dynamic delimiter
     if (numbers.startsWith("//")) {
@@ -10,10 +11,12 @@ export function calculate(numbers: string): number {
 
     numbers.replace(/["\n"]/g, delimiter).split(delimiter).forEach((character: string) => {
         if (Number(character) < 0) {
-            throw new Error(`negative numbers not allowed ${Number(character)}`);
+            negativeNumbers.push(Number(character));
         }
         sum += Number(character);
     });
-    
+
+    if (negativeNumbers.length > 0) throw new Error(`negative numbers not allowed ${negativeNumbers.toString()}`);
+
     return sum;
 }
