b9c561dc986f7b367d220c4fc5eccba6ba1dda08
notrad <anoopbevendcunha@gmail.com>
2024-09-14 23:11:59 +0530

    passing test: negative numbers not allowed -1

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,10 +1,19 @@
 export function calculate(numbers: string): number {
     let sum: number = 0;
     let delimiter: string = ",";
+
+    // Setting dynamic delimiter
     if (numbers.startsWith("//")) {
         delimiter = numbers.charAt(2);
         numbers = numbers.slice(2)
     }
-    numbers.replace(/["\n"]/g, delimiter).split(delimiter).forEach((character: string) => sum += Number(character));
+
+    numbers.replace(/["\n"]/g, delimiter).split(delimiter).forEach((character: string) => {
+        if (Number(character) < 0) {
+            throw new Error(`negative numbers not allowed ${Number(character)}`);
+        }
+        sum += Number(character);
+    });
+    
     return sum;
 }
