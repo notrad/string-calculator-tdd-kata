214073418771c6bcf73f7bf0792b7e63a231c0e3
notrad <anoopbevendcunha@gmail.com>
2024-09-14 22:24:40 +0530

    passing test case: input //

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,5 +1,10 @@
 export function calculate(numbers: string): number {
-    let sum : number = 0;
-    numbers.replace(/["\n"]/g,",").split(",").forEach((character: string) => sum += Number(character));
+    let sum: number = 0;
+    let delimiter: string = ",";
+    if (numbers.startsWith("//")) {
+        delimiter = numbers.charAt(2);
+        numbers = numbers.slice(2)
+    }
+    numbers.replace(/["\n"]/g, delimiter).split(delimiter).forEach((character: string) => sum += Number(character));
     return sum;
 }
