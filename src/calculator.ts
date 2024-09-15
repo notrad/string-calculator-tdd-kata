3c44d315b690b0578196431e0bbe2a114092b580
notrad <anoopbevendcunha@gmail.com>
2024-09-15 14:02:33 +0530

    refactor, passing test cases: handle multiple delimiters with length longer than one char

--- src/calculator.ts
+++ src/calculator.ts
@@ -3,29 +3,29 @@ export function calculate(numbers: string): number {
     let multipleDelimiters: string[] = [];
     let negativeNumbers: number[] = [];
     let parsedNumbers: number[] = [];
-    let indexOfClosingBracket: number = 0;
     const delimiterRegex = /\[(.*?)\]/g;
 
     if (numbers.startsWith("//")) {
         if (numbers.charAt(2) !== "[") {
             singleDelimiter = numbers.charAt(2);
-            numbers = numbers.slice(2);
+            numbers = numbers.slice(4);
         } else {
             multipleDelimiters = [...numbers.matchAll(delimiterRegex)].map(match => match[1]);
-            indexOfClosingBracket = numbers.lastIndexOf("]") + 1;
-            numbers = numbers.slice(indexOfClosingBracket);
+            numbers = numbers.slice(numbers.indexOf('\n') + 1);
         }
     }
 
-
     if (multipleDelimiters.length > 0) {
-        multipleDelimiters.forEach((delimiter: string) => {
-            numbers = numbers.replace(new RegExp(delimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g'), singleDelimiter);
+        multipleDelimiters.forEach(delimiter => {
+            numbers = numbers.split(new RegExp(delimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'))).join(singleDelimiter);
         });
     }
 
-    parsedNumbers = numbers.replace(/["\n"]/g, singleDelimiter).split(singleDelimiter).map(Number).filter((number:number) => number < 1000 || Number.isNaN(number));
-
+    parsedNumbers = numbers
+                        .replace(/\n/g, singleDelimiter)
+                        .split(singleDelimiter)
+                        .map(Number)
+                        .filter((number:number) => number < 1000 || Number.isNaN(number));
 
     negativeNumbers = parsedNumbers.filter((number:number) => number < 0);
 
