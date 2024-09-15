83e151406e80b36821d03f2b53420f04f288424c
notrad <anoopbevendcunha@gmail.com>
2024-09-15 11:56:34 +0530

    passing test case: “//[***]\n1***2***3” should return 6

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,10 +2,19 @@ export function calculate(numbers: string): number {
     let delimiter: string = ",";
     let negativeNumbers: number[] = [];
     let parsedNumbers: number[] = [];
+    let indexOfOpeningBracket: number = 0;
+    let indexOfClosingBracket: number = 0;
 
     if (numbers.startsWith("//")) {
-        delimiter = numbers.charAt(2);
-        numbers = numbers.slice(2)
+        if (numbers.charAt(2) !== "[") {
+            delimiter = numbers.charAt(2);
+            numbers = numbers.slice(2);
+        } else {
+            indexOfOpeningBracket = numbers.indexOf("[") + 1;
+            indexOfClosingBracket = numbers.indexOf("]");
+            delimiter = numbers.slice(indexOfOpeningBracket, indexOfClosingBracket);
+            numbers = numbers.slice(indexOfClosingBracket + 1);
+        }
     }
 
     parsedNumbers = numbers.replace(/["\n"]/g, delimiter).split(delimiter).map(Number).filter((number:number) => number < 1000 || Number.isNaN(number));
